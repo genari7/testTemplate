22eb94446c768f05e12f99e233d36ae9a26e9365
20124899 <35028027+20124899@users.noreply.github.com>
2019-09-04 17:19:48 +0900

    메인단 반응형 세부 조정(2차)
    메인단 반응형 세부 조정(2차)

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+$(function($) {
+    $("body").css("display", "none");
+    $("body").fadeIn(800);
+});
+
 (function($) {
 
 	skel
